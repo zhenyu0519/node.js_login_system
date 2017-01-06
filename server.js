ae63c44a4b4a85c4e1a46db39fb72181efd13355
zhenyu0519 <zhenyu051988@gmail.com>
2017-01-05 17:37:11 -0800

    connected the database and did configuration and added routes

--- server.js
+++ server.js
@@ -8,9 +8,38 @@ var mongoose = reuqire('mongoose');
 var passport = require('passport');
 //Allows for passing session flashdata messages. For example the login error messages
 var flash = require('connect-flash');
+//Create log for every request
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
+//Parse body information from html form 
 var bodyPareser = require('body-parser');
 var session = require('express-session');
 
 var configDB = require('./config/database.js');
+
+//++++++++++++++++configuration+++++++++++++++++++++++++
+//connect the database
+mongoose.connect(configDB.url);
+
+//setup the express application
+
+//create log by the dev formant
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyPareser());
+
+//use the ejs template as view engine
+app.set('view engine', 'ejs')
+
+//required for passport
+app.use(session({secret: 'ilovenodejs'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//+++++++++++++++++++++++route+++++++++++++++++++++++++++++++
+require('./app/routes.js')(app, passport);
+
+//+++++++++++++++++++launch+++++++++++++++++++++++++++++++
+app.listen(port);
+console.log('The server has started on port ' + port);
